502d1dd8fa2212d9a151cf3f8bedcf4c5174c01b
dbal10 <dawid.balcerak10@gmail.com>
2020-08-15 12:45:44 +0200

    InputListener

---
+++ app/src/main/java/com/example/systemy_wbudowane/InputListener.java
@@ -0,0 +1,31 @@
+package com.example.systemy_wbudowane;
+
+import android.view.View;
+
+public class InputListener implements View.OnTouchListener {
+    private static final int SWIPE_MIN_DISTANCE = 0;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 25;
+    private static final int MOVE_THRESHOLD = 250;
+    private static final int RESET_STARTING = 10;
+   //----------------------------------- private final MainView mView;
+    private float x;
+    private float y;
+    private float lastDx;
+    private float lastDy;
+    private float previousX;
+    private float previousY;
+    private float startingX;
+    private float startingY;
+    private int previousDirection = 1;
+    private int veryLastDirection = 1;
+    // Whether or not we have made a move, i.e. the blocks shifted or tried to shift.
+    private boolean hasMoved = false;
+    // Whether or not we began the press on an icon. This is to disable swipes if the user began
+    // the press on an icon.
+    private boolean beganOnIcon = false;
+
+   // public InputListener(MainView view) {
+   //     super();
+  //      this.mView = view;
+   // }
+}
